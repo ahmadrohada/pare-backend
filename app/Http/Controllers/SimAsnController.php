af3dee5a2b6b890ace9ceea00351c69b29c60c9d
ahmadrohada <rohada987@gmail.com>
2021-07-09 12:10:52 +0700

    upadet

--- app/Http/Controllers/SimAsnController.php
+++ app/Http/Controllers/SimAsnController.php
@@ -36,7 +36,7 @@ class SimAsnController extends Controller
             $body = $response->getBody();
             $arr_body = json_decode($body,true);
             //$data = $arr_body;
-            return $arr_body;
+            return $arr_body->access_token;
         }catch(\GuzzleHttp\Exception\GuzzleException $e) {
             return "error";
         }
