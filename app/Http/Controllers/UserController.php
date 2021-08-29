a5c249d90f3a0eab2f5fb75f6c5ecd7de7fbc0e0
ahmadrohada <rohada987@gmail.com>
2021-08-30 06:59:38 +0700

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\RoleUser;
 use App\Models\UserLogging;
 use App\Http\Resources\User as UserResource;
 
@@ -139,12 +140,16 @@ class UserController extends Controller
 
         Paginator::currentPageResolver(fn() => $page );
 
-        $query =  User::select(
-            'username',
-            'id',
-            'nip',
-            'pegawai->nama_lengkap AS nama_lengkap',
-        );
+        $query =  User::
+                        select(
+                            'users.username AS username',
+                            'users.id AS id_user',
+                            'users.nip AS nip',
+                            'users.pegawai->nama_lengkap AS nama_lengkap',
+                            'users.pegawai->jabatan AS jabatan',
+
+                        )
+                        ->orderBY('users.pegawai->jabatan->referensi->id_referensi', 'asc');
 
         if($id_skpd != null ) {
             $query->where('pegawai->skpd->id','=', $id_skpd );
@@ -165,8 +170,77 @@ class UserController extends Controller
             'total'         => $data->total(),
         );
 
+        //pecah  data items untuk nyari is admin atau bukan
+
+        $data = $data->items();
+
+
+        $response = array();
+        foreach( $data AS $x ){
+            if ($x->jabatan ){
+                //diulang sesuai jumlah jabatan
+                foreach( json_decode($x->jabatan) AS $y ){
+                    //data pegawai
+                    $h['id']			= $x->id_user;
+                    $h['username']      = $x->username;
+                    $h['nip']           = $x->nip;
+                    $h['nama_lengkap']  = $x->nama_lengkap;
+                    //data jabatan
+                    $h['jabatan']            = $y->nama;
+                    $h['jabatan_id']         = $y->id;
+
+
+                    //mengetahui jenis jabatan
+                    $jenis_jabatan = isset($y->referensi->referensi->jenis)?($y->referensi->referensi->jenis):null;
+                    $h['jenis_jabatan']     = $jenis_jabatan;
+                    $h['jabatan_referensi_id']  = $y->referensi->id_referensi;
+
+                    if ( $jenis_jabatan == 'struktural'){
+                        //eselon
+                        $h['jabatan_eselon_id']     = $y->referensi->referensi->id;
+                        $h['jabatan_eselon']        = $y->referensi->referensi->eselon->eselon;
+
+                    }else if ( $jenis_jabatan == 'pelaksana'){
+                        $h['jabatan_eselon_id']     = null;
+                        $h['jabatan_eselon']        = null;
+                    }
+
+                    //golongan
+
+                    $h['jabatan_golongan_id']           = isset($y->golongan)?$y->golongan->referensi->id:null;
+                    $h['jabatan_golongan']              = isset($y->golongan)?$y->golongan->referensi->golongan:null;
+                    $h['jabatan_golongan_pangkat']      = isset($y->golongan)?$y->golongan->referensi->pangkat:null;
+
+
+                    //skpd
+                    $h['jabatan_skpd_nama']     = $y->skpd->nama;
+
+
+                    //is admin
+                    $admin = RoleUser::WHERE('user_id',$x->id_user)->WHERE('role_id','2')->exists();
+                    $h['is_admin'] = $admin;
+
+                    //push jika skpd nya relavan dengan yang dicari
+                    $h['jabatan_skpd_id']       = $y->skpd->id;
+                    if( $id_skpd != null ) {
+                        if ( $h['jabatan_skpd_id'] ==  $id_skpd){
+                            array_push($response, $h);
+                        }
+                    }else{
+                        array_push($response, $h);
+                    }
+
+
+                }
+            }
+
+        }
+
+        //$response = collect($response)->sortByDesc('jabatan_golongan_id')->values();
+        $response = collect($response)->sortBy('jabatan_referensi_id')->values();
+
         return [
-            'data'          => $data->items(),
+            'data'          => $response ,
             'pagination'    => $pagination,
 
         ];
